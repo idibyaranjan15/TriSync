d86eaccacd4d40750fcf9622aea5d28a447bc510
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2024-02-15 06:29:07 +0000

    COMMIT

---
+++ Contribute-By-Dibya/15-02-2024/1.js
@@ -0,0 +1,19 @@
+// let body=document.querySelectorAll('body')
+// let button=document.querySelectorAll('button')
+// console.log(button)
+// let li=document.querySelectorAll('li')
+// console.log(li)
+// let ul=document.querySelector('ul')
+
+// button.forEach((ele)=>{
+//     ele.addEventListener('click',(e)=>{
+//         // e.target.style.textDecoration="line-through"
+//         // e.target.innerText+=" clicked"
+//         let list=document.createElement('li')
+//         list.textContent="gym"
+//         ul.prepend(list)
+
+//     })
+// })
+
+
