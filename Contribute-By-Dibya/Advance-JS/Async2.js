7430f792d475f5c1adc518763449ad2c97019768
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2024-02-20 11:00:44 +0000

    learned setTimeout and setInterval and Async js

---
+++ Contribute-By-Dibya/Advance-JS/Async2.js
@@ -0,0 +1,22 @@
+// // setInterval(()=>{
+// //     console.log("DIBYA",Date.now())
+// // },1000)
+// // const sayDate=(str)=>{
+// //     console.log(str,Date.now())
+// // }
+// // const intertvalId=setInterval(sayDate,2000,'hi')
+// // clearInterval(intertvalId)
+// document.querySelector("#start").addEventListener('click', () => {
+//     const sayDate = (str) => {
+//         console.log(str, Date.now());
+//     }
+//     setInterval(sayDate, 1000, 'hi');
+// });
+
+// document.querySelector("#stop").addEventListener('click', () => {
+//     const sayDate = (str) => {
+//         console.log(str, Date.now());
+//     }
+//     const intervalId = setInterval(sayDate, 1000, 'hi');
+//     clearInterval(intervalId);
+// });
