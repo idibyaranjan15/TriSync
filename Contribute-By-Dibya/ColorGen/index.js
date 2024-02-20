b8d87d094f06731ae140cc9a85b4dd7a798aac4a
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2024-02-20 11:18:53 +0000

    Made a color-Generator App using simple HTML and JS

---
+++ Contribute-By-Dibya/ColorGen/index.js
@@ -0,0 +1,24 @@
+const start=document.querySelector('#start')
+const stop=document.querySelector('#stop')
+
+let randomColor=()=>{
+    let hexCode='abcde1234567890';
+    let genColor='#';
+    for(let i=0;i<6;i++){
+        genColor+=hexCode[Math.floor(Math.random()*16)]
+    }
+    return genColor;
+}
+let intervalId;
+const startChange=()=>{
+   intervalId=setInterval(()=>{
+        document.body.style.backgroundColor=randomColor()
+    },100)
+}
+const stopChange=()=>{
+    clearInterval(intervalId)
+}
+
+
+start.addEventListener('click',startChange)
+stop.addEventListener('click',stopChange)
