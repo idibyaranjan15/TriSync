4158eca4bdb118ce9edededd6cd6fa1eecffe4b0
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2024-02-20 08:37:57 +0000

    Learned Advance-JS concept like memoziation,closure,scope

---
+++ Contribute-By-Dibya/Advance-JS/scope.js
@@ -0,0 +1,18 @@
+// {
+//     let a=100;
+//     console.log(a);
+//     // let a=10;//error
+//   // a cant be reintialized
+//   // this is called block-scope
+// }
+// let b=100;
+// {
+//     console.log(b);
+// }
+
+
+
+    var say=()=>console.log(msg);
+    say();
+    var msg='hi';
+    
--- Contribute-By-Dibya/Array&Methods/01_array.js
+++
@@ -1,8 +0,0 @@
-let letters=["a","b","c","d","e"]
-// let rev = letters.reverse();
-// console.log(rev)
-// console.log(letters)
-let nums=[1,2,3,4,5]
-let conacted=nums.concat(letters)
-console.log(...nums,...letters)
-console.log(conacted)
