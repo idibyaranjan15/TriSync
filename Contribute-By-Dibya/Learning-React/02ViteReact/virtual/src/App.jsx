f1fdb212d8180ac4ab0aa6003268db3288c62fac
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2024-03-09 17:08:58 +0000

    learning react

---
+++ Contribute-By-Dibya/Learning-React/02ViteReact/virtual/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Video,Thumb} from "./Video"
+
+function App(){
+  return <div>Hello<Video/><Thumb/></div>
+}
+
+
+
+export default App
---
+++ Contribute-By-Dibya/Learning-React/02ViteReact/virtual/src/Video.jsx
@@ -0,0 +1,11 @@
+function Video(){
+    return (
+    <div>Video</div>
+    )
+}
+const Thumb=()=>{
+    return(
+      <div>Thumb</div>
+    )
+  }
+export  {Video,Thumb};
